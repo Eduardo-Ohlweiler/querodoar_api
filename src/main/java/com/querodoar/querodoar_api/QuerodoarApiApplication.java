6abf0af4535692d522de9e63123200593e2995b4
Eduardo-Ohlweiler <ohlweilereduardo@gmail.com>
2025-08-16 08:54:40 -0300

    first commit

---
+++ src/main/java/com/querodoar/querodoar_api/QuerodoarApiApplication.java
@@ -0,0 +1,13 @@
+package com.querodoar.querodoar_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerodoarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuerodoarApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/querodoar/querodoar_api/QuerodoarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.querodoar.querodoar_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerodoarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
