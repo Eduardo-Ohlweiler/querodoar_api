f9e6ddecd67b21d232a7e464480b7d17b584405a
Kauê Gomes <kaue.gomes@ufrgs.br>
2025-10-05 13:31:16 -0300

    feat: Atualiza configuração de propriedades para suportar variáveis de ambiente e melhora a configuração CORS

--- src/main/java/com/querodoar/querodoar_api/config/CorsConfig.java
+++ src/main/java/com/querodoar/querodoar_api/config/CorsConfig.java
@@ -1,5 +1,8 @@
+// TODO: Rotas públicas não estão passando pela validação do cors
+
 package com.querodoar.querodoar_api.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.CorsFilter;
@@ -9,13 +12,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
+    @Value("${app.frontend.url}")
+    private String frontendUrl;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173") // Domínio específico
+                        .allowedOrigins(frontendUrl) // Domínio específico
                         .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true); // Permite envio de credenciais
